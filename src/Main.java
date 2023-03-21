470c54503c8340c6a37c7a809407ae6c1f050224
ch3yk <bodiga.stas@mail.ru>
2023-03-21 22:24:15 +0300

    выполнил домашку

--- src/Main.java
+++ src/Main.java
@@ -54,9 +54,44 @@ public class Main {
         }
     }
     public static void task4 () {
-        System.out.println("Задача  4");
+        int deliveryDistance = 95;
+        int deliveryTime = 1;
+        if (deliveryDistance<20){
+            System.out.println("Нужно дней: " + deliveryTime);
+        } else if (deliveryDistance<60) {
+            System.out.println("Нужно дней: " + (deliveryTime+1));
+        } else if (deliveryDistance<100) {
+            System.out.println("Нужно дней: " + (deliveryTime+2));
+        } else {
+            System.out.println(" Нет доставки свыше 100 км ");
+        }
     }
     public static void task5 () {
-        System.out.println("Задача  5");
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("неверно указан месяц");
+                break;
+        }
     }
 }
